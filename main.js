43465115a82ff71df22cb5e21ad3b5f77e8502cd
kevin578 <kbriggs@aapl000894.local>
2017-08-27 20:34:08 -0400

    try doing all in one line

---
+++ main.js
@@ -0,0 +1,114 @@
+window.onload = function() {
+
+
+var numbers = [];
+var displayNumber = 0;
+var lastButtonPressed = 'start';
+var lastItem = numbers[numbers.length - 1];
+//set event listeners for buttons
+press('digits', numberButton);
+press('operators', operatorButton);
+press('clear', clearButton);
+press('equals', equalButton)
+
+
+
+function press(className, functionName) {
+	let dig = document.getElementsByClassName(className);
+	for (var i = 0; i < dig.length; i++) {
+			let content = dig[i].innerHTML;
+	    dig[i].addEventListener('click', function() {
+				functionName(content);
+			});
+	}
+}
+
+
+//callbacks once event listeners are set
+
+//numbers
+function numberButton(number) {
+	if (lastButtonPressed == 'operator'){
+		displayNumber = 0;
+	}
+
+	displayNumber = "" + displayNumber + number;
+	displayNumber = parseInt(displayNumber);
+	lastButtonPressed = 'number';
+
+	render(displayNumber);
+
+}
+
+//operators
+function operatorButton(operator) {
+
+	if (lastButtonPressed == 'number') {
+		numbers.push(displayNumber);
+	}
+	else if (lastButtonPressed == 'operator') {
+		numbers[numbers.length - 1] = operator;
+	}
+
+
+	if (numbers.length % 2 != 0 && numbers.length >= 3) {
+		numbers[lastItem] = sumItAll();
+		numbers.push(operator);
+		render(numbers[lastItem]);
+
+
+	}
+	else if (lastButtonPressed == 'number') {
+		numbers.push(operator);
+	}
+
+lastButtonPressed = 'operator';
+
+}
+
+//clear button
+function clearButton(){
+	numbers = [];
+	lastButtonPressed = 'start';
+	displayNumber = 0;
+	render(displayNumber);
+}
+
+
+//equal button
+function equalButton() {
+	if (lastButtonPressed == 'number') {
+		numbers.push(displayNumber);
+		numbers[numbers.length - 1] = sumItAll();
+	}
+	lastButtonPressed = 'equals';
+	render(numbers[numbers.length - 1]);
+}
+
+
+//function to compute last two numbers
+function sumItAll() {
+var y = numbers[numbers.length - 1]
+var op = numbers[numbers.length - 2]
+var x = numbers[numbers.length - 3]
+
+
+	var math_it_up = {
+			'+':  (x, y) => x + y,
+			'-':  (x, y) => x - y,
+			'*':  (x, y) => x * y,
+			'/':  (x, y) => x / y
+		};
+
+		return math_it_up[op](x,y);
+
+
+}
+
+//render
+function render(content) {
+document.getElementById("screen").getElementsByTagName("p")[0].innerHTML = content;
+}
+
+
+}
